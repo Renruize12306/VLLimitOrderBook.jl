b672e8733d7747ab70f6fc8f1884892f0406d803
Ruize <renruize1998@gmail.com>
2023-03-24 07:54:20 -0400

    changes for running fig test

--- test/runn_fig_test.jl
+++ test/runn_fig_test.jl
@@ -1,10 +1,11 @@
 begin
     arr = [
-        "test-2_time_num_limit_fig.jl",
-        "test-2_time_num_market_fig.jl",
+        # "validation/book_validation_benchmark.jl",
+        # "test-2_time_num_limit_fig.jl",
+        # "test-2_time_num_market_fig.jl",
         "stress_test_conc_20_pro_act_fig.jl",
-        "stress_test_conc_20_pro_avg_fig.jl",
-        "stress_test_conc_num_proc_fig.jl"
+        # "stress_test_conc_20_pro_avg_fig.jl",
+        # "stress_test_conc_num_proc_fig.jl",
         ]
     for file in arr
         println("Started\t", file)
