04f0157105c23710cac07234d2dc784b1d558dc9
Renruize12306 <renruize1998@gmail.com>
2023-03-28 14:39:48 -0400

    adding insertion test

--- test/figures_input/runn_fig_test.jl
+++ test/figures_input/runn_fig_test.jl
@@ -1,11 +1,12 @@
 begin
     arr = [
         "utility.jl",
-        "test_shape_fig.jl",
-        "test-2_time_num_limit_fig.jl",
-        "test-2_time_num_market_fig.jl",
-        "stress_test_conc_20_pro_fig.jl",
-        "stress_test_conc_num_proc_fig.jl",
+        "test_insertion.jl",
+        # "test_shape_fig.jl",
+        # "test-2_time_num_limit_fig.jl",
+        # "test-2_time_num_market_fig.jl",
+        # "stress_test_conc_20_pro_fig.jl",
+        # "stress_test_conc_num_proc_fig.jl",
         ]
     for file in arr
         println("Started\t", file)
