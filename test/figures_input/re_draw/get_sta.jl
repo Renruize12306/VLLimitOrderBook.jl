6271d5cf4e58c189d20d571ef72b0584921ae935
Renruize12306 <renruize1998@gmail.com>
2023-03-30 10:38:40 -0400

    add some drawing script

---
+++ test/figures_input/re_draw/get_sta.jl
@@ -0,0 +1,42 @@
+
+using Plots
+function assing_valiables(scale, dir)
+    test_shape_fig_x_file = open("$(dir)/test_insertion_x.txt")
+    test_shape_fig_y_file = open("$(dir)/test_insertion_y.txt")
+    
+    x_vec = Vector{Any}()
+    y_vec = Vector{Any}()
+    cnt = 1
+    curline_fig_x_vec = "@"
+    while true
+        curline_fig_x_vec = readline(test_shape_fig_x_file)
+        curline_fig_y_vec = readline(test_shape_fig_y_file)
+        if curline_fig_x_vec == ""
+            break
+        end
+        if cnt < scale # == 0
+            push!(x_vec, parse(Float64, curline_fig_x_vec))
+            push!(y_vec, parse(Float64, curline_fig_y_vec))
+        end
+        cnt += 1
+    end
+    return [x_vec, y_vec]
+end
+
+dir = pwd() * "/test/fig/mac_test_insert/test_insertion"
+result_vec = assing_valiables(1000_000, dir)
+
+vec = result_vec[2]
+MEAN_VALUE = sum(vec) / length(vec)
+MAX_VALUE = maximum(vec)
+MIN_VALUE = minimum(vec)
+
+function my_std(samples)
+    samples_mean = sum(vec) / length(vec)
+    samples_size = length(samples)
+    samples = map(x -> (x - samples_mean)^2, samples)
+    samples_sum = sum(samples)
+    samples_std = sqrt(samples_sum / (samples_size - 1))
+    return samples_std
+end
+STD_VALUE = my_std(vec)
---
+++ test/figures_input/re_draw/shape_fig.jl
@@ -0,0 +1,42 @@
+using Plots
+function assing_valiables(scale, dir)
+    test_shape_fig_x_file = open("$(dir)/test_shape_fig_x.txt")
+    test_shape_fig_y_file = open("$(dir)/test_shape_fig_y.txt")
+    
+    x_vec = Vector{Any}()
+    y_vec = Vector{Any}()
+    cnt = 1
+    curline_fig_x_vec = "@"
+    while true
+        curline_fig_x_vec = readline(test_shape_fig_x_file)
+        curline_fig_y_vec = readline(test_shape_fig_y_file)
+        if curline_fig_x_vec == ""
+            break
+        end
+        if cnt < scale # == 0
+            push!(x_vec, parse(Float64, curline_fig_x_vec))
+            push!(y_vec, parse(Float64, curline_fig_y_vec))
+        end
+        cnt += 1
+    end
+    return [x_vec, y_vec]
+end
+
+dir = pwd() * "/test/test_shape_fig"
+result_vec = assing_valiables(1000_000, dir)
+
+x_array = result_vec[1]
+# x_array = x_array[1 : end - 1]
+y_array = result_vec[2]
+# y_array_sent = y_array_sent[1 : end - 1]
+
+scatter(x_array, y_array, label="Performance", mc=:white, msc=colorant"#EF4035", legend=:best, 
+bg="floralwhite", background_color_outside="white", framestyle=:box, fg_legend=:transparent, lw=3)
+xlabel!("Order Book Depth", fontsize=18)
+ylabel!("Processing Time (seconds)", fontsize=18)
+
+
+savefig("$(dir)/pic.png")
+
+
+# include("test/figures_input/re_draw/shape_fig.jl")
