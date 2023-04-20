10dc5c79e32cacd5022d5eddfc52c492844a19e2
Renruize12306 <renruize1998@gmail.com>
2023-04-20 16:31:41 -0400

    change the latency to nanoseconds

--- test/figures_input/simpletest/sptest.jl
+++ test/figures_input/simpletest/sptest.jl
@@ -10,5 +10,16 @@ sleep(1)
 t2 = now().instant.periods.value;
 println(t2-t1)
 end
+
+
+t = @elapsed begin
+    t1 = Int(time_ns());
+    sleep(1)
+    t2 = Int(time_ns());
+
+    println(t2-t1)
+end
+
+
 # 1000 milliseconds = 1 second
 # include("test/figures_input/simpletest/sptest.jl")
