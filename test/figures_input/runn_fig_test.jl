9a7512fdedf5990382667097370b2790a26056a1
Renruize12306 <61093011+Renruize12306@users.noreply.github.com>
2023-05-08 13:58:37 -0400

    Revert "adding comparation with BSE and CoinTossX"
    This reverts commit 600eaed52e8721986aea58a6cfb15d4937814172.

--- test/figures_input/runn_fig_test.jl
+++ test/figures_input/runn_fig_test.jl
@@ -1,10 +1,9 @@
 begin
     arr = [
         "utility.jl",
-        # "test_insertion.jl",
+        "test_insertion.jl",
         # "test_shape_fig.jl",
         # "test-2_time_num_limit_fig.jl",
-        "test-2_time_num_limit_fig_cmp.jl",
         # "test-2_time_num_market_fig.jl",
         # "stress_test_conc_20_pro_fig.jl",
         # "stress_test_conc_num_proc_fig.jl",
