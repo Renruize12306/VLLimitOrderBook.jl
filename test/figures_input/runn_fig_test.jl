0f443ca4dbde020dffc283b8768b1493b479b659
Renruize12306 <61093011+Renruize12306@users.noreply.github.com>
2023-05-08 13:58:37 -0400

    Revert "modified insert positions"
    This reverts commit 546e60175391b2acd01f372bd626bec679b4dd52.

--- test/figures_input/runn_fig_test.jl
+++ test/figures_input/runn_fig_test.jl
@@ -1,13 +1,13 @@
 begin
     arr = [
         "utility.jl",
-        "test_insertion.jl",
+        # "test_insertion.jl",
         # "test_shape_fig.jl",
         # "test-2_time_num_limit_fig.jl",
         # "test-2_time_num_market_fig.jl",
         # "stress_test_conc_20_pro_fig.jl",
         # "stress_test_conc_num_proc_fig.jl",
-        # "../validation/book_validation_visualization.jl",
+        "../validation/book_validation_visualization.jl",
         ]
     for file in arr
         println("Started\t", file)
