c981728faac18cc41e75db2b358a88c61b2f847e
Renruize12306 <61093011+Renruize12306@users.noreply.github.com>
2023-05-08 13:58:37 -0400

    Revert "add visualization"
    This reverts commit 1bbbedb148f4e0e4c68e619ec852133486ae1adb.

--- test/figures_input/runn_fig_test.jl
+++ test/figures_input/runn_fig_test.jl
@@ -5,9 +5,8 @@ begin
         # "test_shape_fig.jl",
         # "test-2_time_num_limit_fig.jl",
         # "test-2_time_num_market_fig.jl",
-        # "stress_test_conc_20_pro_fig.jl",
+        "stress_test_conc_20_pro_fig.jl",
         # "stress_test_conc_num_proc_fig.jl",
-        "../validation/book_validation_visualization.jl",
         ]
     for file in arr
         println("Started\t", file)
